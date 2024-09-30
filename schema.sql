6095edcc56b1bd6d432f1434e4adb1b87327a8bd
Ryan <eskridgeryan@gmail.com>
2024-09-30 09:55:42 -0500

    Converted DBML to sql using Sqlite preset. Removed test.txt

---
+++ schema.sql
@@ -0,0 +1,78 @@
+-- SQL dump generated using DBML (dbml.dbdiagram.io)
+-- Database: PostgreSQL
+-- Generated at: 2024-09-30T14:54:55.535Z
+
+CREATE TABLE "users" (
+  "id" integer UNIQUE PRIMARY KEY,
+  "username" varchar NOT NULL,
+  "role" varchar DEFAULT 'user',
+  "created_at" timestamp NOT NULL
+);
+
+CREATE TABLE "campaign" (
+  "id" integer UNIQUE PRIMARY KEY,
+  "title" varchar DEFAULT 'Campaign Title',
+  "notes" text DEFAULT null,
+  "user_id" integer,
+  "status" bool DEFAULT false,
+  "created_at" timestamp NOT NULL
+);
+
+CREATE TABLE "test" (
+  "id" integer UNIQUE PRIMARY KEY,
+  "title" varchar DEFAULT 'Test Name',
+  "content" varchar,
+  "sender" varchar,
+  "camp_id" integer,
+  "status" bool DEFAULT false,
+  "created_at" timestamp NOT NULL
+);
+
+CREATE TABLE "targets" (
+  "id" integer UNIQUE PRIMARY KEY,
+  "name" varchar NOT NULL,
+  "email_addr" varchar NOT NULL
+);
+
+CREATE TABLE "test_targets" (
+  "test_id" integer,
+  "target_id" integer,
+  "clicked" bool DEFAULT false,
+  "clicked_at" timestamp DEFAULT null
+);
+
+COMMENT ON COLUMN "users"."role" IS 'will we even have multiple roles?';
+
+COMMENT ON COLUMN "campaign"."status" IS 'Might need more than a bool';
+
+COMMENT ON COLUMN "test"."content" IS 'Email content';
+
+COMMENT ON COLUMN "test"."sender" IS 'Sender address';
+
+COMMENT ON COLUMN "test"."status" IS 'Might need more than a bool';
+
+ALTER TABLE "users" ADD FOREIGN KEY ("id") REFERENCES "campaign" ("user_id");
+
+ALTER TABLE "campaign" ADD FOREIGN KEY ("id") REFERENCES "test" ("camp_id");
+
+CREATE TABLE "test_test_targets" (
+  "test_id" integer,
+  "test_targets_test_id" integer,
+  PRIMARY KEY ("test_id", "test_targets_test_id")
+);
+
+ALTER TABLE "test_test_targets" ADD FOREIGN KEY ("test_id") REFERENCES "test" ("id");
+
+ALTER TABLE "test_test_targets" ADD FOREIGN KEY ("test_targets_test_id") REFERENCES "test_targets" ("test_id");
+
+
+CREATE TABLE "targets_test_targets" (
+  "targets_id" integer,
+  "test_targets_target_id" integer,
+  PRIMARY KEY ("targets_id", "test_targets_target_id")
+);
+
+ALTER TABLE "targets_test_targets" ADD FOREIGN KEY ("targets_id") REFERENCES "targets" ("id");
+
+ALTER TABLE "targets_test_targets" ADD FOREIGN KEY ("test_targets_target_id") REFERENCES "test_targets" ("target_id");
+
